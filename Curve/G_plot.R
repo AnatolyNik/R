9f7647a6b1ff6bdee013845db73ee61fb88801d0
AnatolyNik <a_nik@hotmail.com>
2017-11-12 13:30:40 +0300

    изм

--- Curve/G_plot.R
+++ Curve/G_plot.R
@@ -4,12 +4,18 @@ library("ggplot2")  # графики
 
 d_ <- read.csv("data.csv")
 head(d_)
+summary(d_)
 
 t <- d_[2]
 l <- d_[3]
 l_curve <-  d_[3]
 
-ggplot(data = d_, aes(x=t, y=l_curve))  + geom_point()
-ggplot(data = d_, aes(x=t, y=l_curve, color = l_curve))  + stat_identity()
-ggplot(data = d_, aes(x=t>5, y=l_curve, color = l_curve))  + geom_boxplot()
+ggplot(data = d_, aes(x = t, y = l_curve))  + geom_point()
+ggplot(data = d_, aes(x = t, y = l_curve, color = l_curve))  + stat_identity() + 
+  xlab("Время испытания, сек") +
+  ylab("Значение, Ра") +
+  scale_color_continuous("Значение") +
+  ggtitle("Зависимость давления от времени") 
+
+ggplot(data = d_, aes(x = t > 5, y = l_curve, color = l_curve))  + geom_boxplot() 
 
